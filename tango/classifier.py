6202841f1fb0e126c404280c13e025292f91448e
whatthefua <arnan_s@msn.com>
2018-10-21 11:43:12 +0200

    updated classifier.py

--- tango/classifier.py
+++ tango/classifier.py
@@ -46,7 +46,7 @@ def runOptimize(epochs):
 		print("Validation loss = %lf" % (iterationLoss / float(len(valid))),)
 		print("Validation accuracy = %lf" % (accuracy / float(len(valid))))
 
-def runTest():
+def runTest(epoch):
 	global test
 	global model_x, model_y_true, model_y_pred, model_loss, model_opt, model_sess
 
@@ -54,15 +54,22 @@ def runTest():
 	iterationLoss = 0.0
 	accuracy = 0.0
 
+	fp = open("log-%d.txt" % epoch, "w")
+
 	for i in range(0, len(test), 64):
 		lowerIdx = i
 		upperIdx = min(i + 64, len(test))
 
 		feedDict = { model_x: [x[0] for x in test[lowerIdx: upperIdx]], model_y_true: [[x[1]] for x in test[lowerIdx: upperIdx]] }
-		[loss, acc] = model_sess.run([model_loss, model_accuracy], feed_dict = feedDict)
+		[loss, acc, y_pred, y_true] = model_sess.run([model_loss, model_accuracy, model_y_pred, model_y_true], feed_dict = feedDict)
 
 		iterationLoss += (upperIdx - lowerIdx) * loss
 		accuracy += (upperIdx - lowerIdx) * acc
+
+		for j in range(upperIdx - lowerIdx):
+			fp.write("%lf %lf\n" % (y_pred[j], y_true[j]))
+
+	fp.close()
 
 	print("Testing loss = %lf" % (iterationLoss / float(len(test))))
 	print("Testing accuracy = %lf" % (accuracy / float(len(test))))
@@ -72,7 +79,7 @@ train, valid, test = load_data.load_data([9, 11, 50, 49, 10, 60, 57, 58, 59, 82,
 # train, valid, test = load_data.load_data([11, 50, 49, 10, 60, 57, 58, 59, 82, 12, 7, 90, 64, 53, 25, 13, 15, 63, 83, 34])
 # train, valid, test = load_data.load_data([3, 5, 9, 11, 50, 49, 10, 60, 57, 58, 59, 82, 12, 7, 90, 64, 53, 25, 13, 15, 63, 83, 34, 38, 46, 43, 36, 91, 92, 14, 81, 85, 80, 84, 55, 93, 94, 42, 20, 24, 45, 44, 54, 8, 6, 41, 74, 23, 22, 47, 48, 33, 27, 26, 73, 51, 16, 95, 35, 88, 76, 37, 68, 17, 65, 86, 18, 21, 72, 31, 71, 66, 79, 67, 29, 77, 78, 89, 28, 30, 87, 32, 19, 52, 56])
 
-train = train + valid
+# train = train + valid
 
 model_x = tf.placeholder(tf.float32, shape = [None, 45])
 model_y_true = tf.placeholder(tf.float32, shape = [None, 1])
@@ -94,4 +101,4 @@ model_sess.run(tf.global_variables_initializer())
 
 for epoch in range(10):
 	runOptimize(1)
-	runTest()
+	runTest(epoch)
